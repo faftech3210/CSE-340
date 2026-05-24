30b7ee93d37f4e21d8a56b32e27704df097382bf
faftech3210 <franklinakor@gmail.com>
2026-05-24 16:55:21 +0100

    W3 asignment

--- src/routes.js
+++ src/routes.js
@@ -3,19 +3,26 @@ import express from 'express';
 import { showHomePage } from './controllers/index.js';
 import { showOrganizationsPage } from './controllers/organizations.js';
 import { showProjectsPage, showProjectDetailsPage } from './controllers/projects.js';
-import { showCategoriesPage } from './controllers/categories.js';
+import { showCategoriesPage, showCategoryDetailsPage } from './controllers/categories.js';
 import { testErrorPage, show500Page, show404Page } from './controllers/errors.js';
 import { showOrganizationDetailsPage } from './controllers/organizations.js';
 
 const router = express.Router();
 
-//Route for pages rendering
-router.get('/', showHomePage);
-router.get('/organizations', showOrganizationsPage);
-router.get('/projects', showProjectsPage);
-router.get('/categories', showCategoriesPage);
-router.get('/organization/:id', showOrganizationDetailsPage);
-router.get('/project/:id', showProjectDetailsPage);
+// Route for pages rendering
+router.get("/", showHomePage)
+
+router.get("/organizations", showOrganizationsPage)
+
+router.get("/organization/:id", showOrganizationDetailsPage)
+
+router.get("/projects", showProjectsPage)
+
+router.get("/project/:id", showProjectDetailsPage)
+
+router.get("/categories", showCategoriesPage)
+
+router.get("/categories/:id", showCategoryDetailsPage)
 
 // error-handling routes
 router.get('/test-error', testErrorPage);
